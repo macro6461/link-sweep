b0cfcfa8825d24f9e4fa23865f0927f7cb342450
Matthew Croak <mattcroak718@gmail.com>
2023-03-08 10:50:25 -0500

    added popup helper to handle errors and show whether or not LinkSweep has access to site

--- sweep.js
+++ sweep.js
@@ -1,20 +1,16 @@
 gatherUrls = (type) =>{
-
     const functionsObj = {
         'medium': mediumHelper,
         'blogger': bloggerHelper
     }
-
     const linkObj = {}
     const helper = functionsObj[type]
     const links = document.links
-
     for (let i = 0; i < links.length; ++i){
         let link = links[i]
         link = helper(link)
         linkObj[link] = link
     }
-
     return Object.values(linkObj).join("\n\n").trim()
 }
 
@@ -50,6 +46,7 @@ updateClipboard = async (content) => {
             createPopUp()
         },
         (err) => {
+            createPopUp(false, err)
             console.log(err)
         }
     );
@@ -59,16 +56,46 @@ removeSelf = (e) => {
     e.target.parentElement.parentElement.remove()
 }
 
-createPopUp = (type) => {
-    const html = `
-        <div id="link-sweep-popup">
-        <button id="close-link-sweep">x</button>
+popupHelper = (isNotAvailable, err) => {
+    let middle = `
             <div>
                 <p>Links copied to clipboard!</p>
                 <p>If you liked this tool, please leave a tip or follow me on Medium!</p>
                 <p>Please report any bugs on Github.</p>
                 <p>Thanks for using Link Sweep!</p>
             </div>
+    `
+    if (isNotAvailable){
+        middle = `
+            <div>
+                <p>LinkSweep does not have access to this website. :(</p>
+                <p>This extension currently only works for Medium and Blogger.</p>
+                <p>Please report any bugs on Github.</p>
+                <p>Thanks for using Link Sweep!</p>
+            </div>
+        `
+    }
+
+    if (err){
+        middle = `
+        <div>
+            <p>Could not copy links. :(</p>
+            <p style="color: red;">${err}</p>
+            <p>If Document is not focused, simply click in the post and try again.</p>
+            <p>Thanks for using Link Sweep!</p>
+        </div>
+    `
+    }
+
+    return middle
+}
+
+createPopUp = (isNotAvailable, err) => {
+    const middle = popupHelper(isNotAvailable, err)
+    const html = `
+        <div id="link-sweep-popup">
+        <button id="close-link-sweep">x</button>
+            ${middle}
             <a href="https://paypal.me/mattcroak?country.x=US&amp;locale.x=en_US" target="_blank">Paypal</a>
             <a href="https://matt-croak.medium.com/membership" target="_blank">Medium</a>
             <a href="https://www.vecteezy.com/free-vector/broom-icon" target="_blank">Broom Icon Vectors by Vecteezy</a>
@@ -85,7 +112,6 @@ createPopUp = (type) => {
 }
 
 sweep = async () => {
-
     // be sure to remove any remaining popups
     var orphan = document.getElementById('link-sweep-popup-outer')
     if (orphan) orphan.remove()
@@ -97,21 +123,21 @@ sweep = async () => {
     if (matched.length > 0){
         try {
             const hasPermissions = await permissionsCheck();
-    
             if (hasPermissions) {
-    
                 const contentLinks = gatherUrls(matched[0]);
                 if (contentLinks.length) {
                     updateClipboard(contentLinks)
                 }
-    
             } else {
                 alert(`In order to use this extension, you need to enable access to your clipboard.\n
                 If you did enable access, please click the icon again. :)`);
             }
         } catch (err) {
             console.error(err);
+            createPopUp(false, err)
         }
+    } else {
+        createPopUp(true)
     }
 }
 
